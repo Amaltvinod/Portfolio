d71ef6c38221917e04d4ced4835aac24457a6bb7
Amaltvinod <amaltvinod5@gmail.com>
2023-03-11 20:08:23 +0530

    completed the desktop home page

--- src/components/Home/Container.jsx
+++ src/components/Home/Container.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar } from "./Navbar";
 import { Profile } from "./Profile";
 import { Dynamic } from "./Dynamic";
 import { Wrapper } from "./ContainerStyles";
 export const Container=()=>{
+    const [sources,setSources]=useState(["/images/right-arrow.png","/images/left-arrow.png"]);
+    const clickHandler=(src)=>{
+        if(src==0){
+            setSources([
+                "/images/right-arrow-active.png",
+                "/images/left-arrow.png"
+            ])
+        }else{
+            setSources([
+                "/images/right-arrow.png",
+                "/images/left-arrow-active.png"
+            ])
+        }
+
+    }
     return(
    <Wrapper>
         <Navbar/>
         <Profile/>
         <Dynamic/>
+        <div className="page-changer">
+            <img src={sources[0]} onClick={()=>clickHandler(0)}></img>
+            <img src={sources[1]} onClick={()=>clickHandler(1)}></img>
+        </div>
     
    </Wrapper>
      
--- src/components/Home/ContainerStyles.js
+++ src/components/Home/ContainerStyles.js
@@ -8,10 +8,36 @@ export const Wrapper=styled.div`
    border:none;
    margin:0;
    padding:0;
+   position:relative;
    
    display:flex;
    flex-direction:row;
    align-items:center;
+   .page-changer{
+      position: absolute;
+      bottom:2em;
+      right:2em;
+      display: flex;
+      flex-direction: column;
+      align-items:center;
+      justify-content:center;
+      border:2px solid #04B4E0;
+      border-radius:20px 20px 20px 20px;
+      background-color:#444444;
+      z-index:2;
+      width: 4em;
+      height: 7em;
+
+      img{
+         max-height:30px;
+         max-width:30px;
+
+      }
+      img:first-child{
+         margin-bottom:1.5em;
+
+      }
+   }
   
    
 `
